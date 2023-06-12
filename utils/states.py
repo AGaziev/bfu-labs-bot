b190e2e331d56acdbc1176fd4453fa6404013c43
AGaziev <alan.gaziev27@gmail.com>
2023-06-12 19:47:02 +0300

    states.py: add FSM states for teacher's group adding form

--- utils/states.py
+++ utils/states.py
@@ -6,3 +6,14 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class registrator(StatesGroup):
     name = State()
+
+
+class AddGroup(StatesGroup):
+    name = State()
+    students = State()
+
+
+class Teacher(StatesGroup):
+    start = State()
+    add_group = AddGroup()
+    show_groups = State()
