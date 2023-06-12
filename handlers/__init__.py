24833f4baae12c45819c00993d700bea83923c9b
AGaziev <alan.gaziev27@gmail.com>
2023-06-12 19:52:53 +0300

    registers for existed teacher handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
 from .user import *
 from .admin import *
+from .teacher import *
 
 # DON'T TOUCH THIS IMPORT
 from loader import dispatcher
@@ -14,17 +15,42 @@ def setup(dp:dispatcher):
     """
 
     """user handlers"""
-    dp.register_message_handler(
-        cmd_start,
-        commands=['start'],
-        state=None)
-
-    dp.register_message_handler(
-        cmd_register_found_user,
-        state=[states.registrator.name])
+    # dp.register_message_handler(
+    #     cmd_start,
+    #     commands=['start'],
+    #     state=None)
+    #
+    # dp.register_message_handler(
+    #     cmd_register_found_user,
+    #     state=[states.registrator.name])
 
     """moder handlers"""
+    # dp.register_message_handler(
+    #     cmd_info,
+    #     commands=["info"],
+    #     state=None)
+
+    """teacher handlers"""
     dp.register_message_handler(
-        cmd_info,
-        commands=["info"],
-        state=None)
+        teacher.login,
+        commands=['start'],
+        state=None
+    )
+
+    dp.register_callback_query_handler(
+        teacher.set_new_group_name,
+        text="add_new_group",
+        state=states.Teacher.start
+    )
+
+    dp.register_message_handler(
+        teacher.set_new_group_students,
+        state=states.Teacher.add_group.name
+    )
+
+    dp.register_message_handler(
+        teacher.end_group_add,
+        content_types=['document'],
+        state=states.Teacher.add_group.students
+    )
+
