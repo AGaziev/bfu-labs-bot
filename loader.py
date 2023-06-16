d66b06d4d025e7956b5ddebe510475ecbffb5b94
PheeZz <jagubkoff@gmail.com>
2023-06-16 20:58:53 +0200

    fix: :ambulance: fix import

--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from data import config
+from data import configuration
 
-bot = Bot(token=config.bot_token)
+bot = Bot(token=configuration.bot_token)
 storage = MemoryStorage()
 dispatcher = Dispatcher(bot, storage=storage)
