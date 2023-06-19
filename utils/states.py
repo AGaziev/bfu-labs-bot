05810bc5cfd9b17a6e5f7e4d552e8744ea9c1606
AGaziev <alan.gaziev27@gmail.com>
2023-06-19 23:35:22 +0300

    feat: 🏗️ states.py: student start+connect group states

--- utils/states.py
+++ utils/states.py
@@ -16,4 +16,12 @@ class AddGroup(StatesGroup):
 class Teacher(StatesGroup):
     start = State()
     add_group = AddGroup()
+
+class ConnectGroup(StatesGroup):
+    group_name = State()
+    choose_name = State()
+
+class Student(StatesGroup):
+    start = State()
+    connect_to_group = ConnectGroup()
     show_groups = State()
