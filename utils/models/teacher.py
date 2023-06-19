31345424b0305f60e2268288bb85fcccf5b58d64
PheeZz <jagubkoff@gmail.com>
2023-06-19 20:36:28 +0200

    docs: :memo: add description for teacher model

--- utils/models/teacher.py
+++ utils/models/teacher.py
@@ -3,7 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Teacher:
-    """dataclass representing a teacher
+    """
+    dataclass representing a teacher
+        fields:
+                firstname: str
+                lastname: str
+                patronymic: str
+                username: str
     """
     firstname: str
     lastname: str
