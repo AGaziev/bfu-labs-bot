9181885dfdb73ab15c78b1a8327c1a9f072ce919
AGaziev <alan.gaziev27@gmail.com>
2023-06-25 19:26:50 +0300

    feat: lab.py: student labs

--- managers/lab.py
+++ managers/lab.py
@@ -1,2 +1,14 @@
+from loguru import logger
+
+from utils.enums import Blocked
+from utils.mailer import Mailer
+from .cloud import CloudManager
+from .db import database_manager
+
 class LabManager:
-    pass
+    @staticmethod
+    def get_student_lab_stats(group_id, telegram_id):
+        lab_statistic = await database_manager.select_students_labs_with_status_in_group(group_id, telegram_id)
+        for lab_info in lab_statistic:
+            pass
+
