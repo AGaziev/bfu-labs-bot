a964b880d4057b34038ca13a0af401eeffe45796
AGaziev <alan.gaziev27@gmail.com>
2023-06-25 14:23:03 +0300

    refactor: connect_to_group.py: delete unused imports + now use formatter for student list + cancel kb

--- handlers/student/connect_to_group.py
+++ handlers/student/connect_to_group.py
@@ -1,20 +1,15 @@
-import io
-
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from aiogram.utils.exceptions import MessageNotModified
-from aiogram.utils.markdown import escape_md
 
-import keyboards
-from data import config
 import keyboards as kb
-from middlewares import rate_limit
 from utils import states
+from utils import Formatter
 from managers import GroupManager
 
 
 async def set_connecting_group_name(call: types.CallbackQuery):
-    await call.message.answer("Напишите название группы к которой хотите подключиться")
+    await call.message.answer("Напишите название группы к которой хотите подключиться",
+                              reply_markup=await kb.cancel_kb())
     await call.message.delete()
     await states.Student.connect_to_group.group_name.set()
 
@@ -26,11 +21,13 @@ async def show_student_list_of_group(message: types.Message, state: FSMContext):
             connecting_data["students"] = \
                 await GroupManager.get_unregistered_users_of_group(group_name=message.text)
             # формируем список можно будет переделать потом красиво и понятно
-            await message.answer("\n".join([f"{i}. {s}" for i, s in connecting_data["students"].items()]))
-            await message.answer("Выберите из списка введя число перед вашим ФИО")
+            await message.answer(Formatter.list_of_students(connecting_data["students"]))
+            await message.answer("Выберите из списка введя число перед вашим ФИО",
+                                 reply_markup=await kb.cancel_kb())
             await states.Student.connect_to_group.choose_name.set()
     else:
-        await message.answer("Такой группы нет, уточните у преподавателя и напишите название еще раз")
+        await message.answer("Такой группы нет, уточните у преподавателя и напишите название еще раз",
+                             reply_markup=await kb.cancel_kb())
 
 
 async def choosing_student(message: types.Message, state: FSMContext):
@@ -44,14 +41,15 @@ async def choosing_student(message: types.Message, state: FSMContext):
                                  f"Если нет, то напишите еще раз число", reply_markup=await kb.confirmation_kb())
         except (ValueError, KeyError):
             await message.answer("Такого номера нет, напишите еще раз число")
-            await message.answer("\n".join([f"{i}. {s}" for i, s in connecting_data["students"].items()]))
+            await message.answer(Formatter.list_of_students(connecting_data["students"]),
+                                 reply_markup=await kb.cancel_kb())
 
 
 async def end_connecting_to_group(call: types.CallbackQuery, state: FSMContext):
     async with state.proxy() as connecting_data:
         await GroupManager.connect_student_to_group(connecting_data['group_name'],
-                                              connecting_data['student_name'],
-                                              connecting_data['student_id'],
-                                              call.from_user.id)
+                                                    connecting_data['student_name'],
+                                                    connecting_data['student_id'],
+                                                    call.from_user.id)
         await call.message.answer(f"Вы подключены к группе {connecting_data['group_name']}")
         await state.finish()
