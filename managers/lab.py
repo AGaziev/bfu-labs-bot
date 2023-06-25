5e65ab3d7ef8bd12b9299812c436999fa1300e19
AGaziev <alan.gaziev27@gmail.com>
2023-06-25 19:43:37 +0300

    feat: lab.py: student labs first vers

--- managers/lab.py
+++ managers/lab.py
@@ -5,10 +5,16 @@ from utils.mailer import Mailer
 from .cloud import CloudManager
 from .db import database_manager
 
+
 class LabManager:
     @staticmethod
-    def get_student_lab_stats(group_id, telegram_id):
+    def get_student_lab_stats(group_id, telegram_id) -> ([dict], [dict]):
         lab_statistic = await database_manager.select_students_labs_with_status_in_group(group_id, telegram_id)
+        accepted_labs = []
+        not_done_labs = []
         for lab_info in lab_statistic:
-            pass
-
+            if lab_info["status"] == "Сдано":
+                accepted_labs.append(lab_info)
+            else:
+                not_done_labs.append(lab_info)
+        return (accepted_labs, not_done_labs)
