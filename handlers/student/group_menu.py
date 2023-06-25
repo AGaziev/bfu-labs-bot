becba5ec4b5e315e96cb619d2b9318faeadf4f7e
AGaziev <alan.gaziev27@gmail.com>
2023-06-26 00:10:05 +0300

    refactor: remove getting group name directly from db func, replaced with encapsulated in group manager

--- handlers/student/group_menu.py
+++ handlers/student/group_menu.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import FSMContext
 import keyboards as kb
 from middlewares import rate_limit
 from aiogram.utils.markdown import hbold
-from managers import database_manager
+from managers import GroupManager
 from utils import states, Formatter
 from managers import LabManager
 
@@ -11,8 +11,10 @@ from managers import LabManager
 async def student_group_menu(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
     # call.data sample: "group:1:student"
     group_id = callback_data["group_id"]
-    lab_stats = LabManager.get_student_lab_stats(group_id, call.from_user.id)
-    group_name = await database_manager.select_group_name_by_group_id(group_id=group_id)
+    async with state.proxy() as data:
+        data["group_id"] = group_id
+    lab_stats = await LabManager.get_student_lab_stats(group_id, call.from_user.id)
+    group_name = await GroupManager.get_group_name_by_id(group_id)
     await call.message.edit_text(f"{hbold(group_name)}\n"
                                  f"{Formatter.group_menu_lab_stats(lab_stats)}",
                                  reply_markup=await kb.student_group_menu_kb(group_id=group_id),
