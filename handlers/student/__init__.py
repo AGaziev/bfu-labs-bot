c17d8bdd5d4b9ee9728480822067ec126f924c5e
AGaziev <alan.gaziev27@gmail.com>
2023-06-26 00:15:48 +0300

    feat: register handlers for student menu

--- handlers/student/__init__.py
+++ handlers/student/__init__.py
@@ -3,6 +3,7 @@ from loguru import logger
 
 from .connect_to_group import set_connecting_group_name, show_student_list_of_group, choosing_student, end_connecting_to_group
 from .show_my_groups import show_my_groups
+from .group_menu import student_group_menu, show_not_done_labs_files
 from utils import states
 from utils import callbacks
 
@@ -33,7 +34,19 @@ def setup_student_handlers(dp: Dispatcher):
 
     dp.register_callback_query_handler(
         show_my_groups,
-        callbacks.show_callback.filter(),#data_type="group", user_type="student")
+        callbacks.show_callback.filter(data_type="group", user_role="student"),
         state=states.Student.start
+    )
+
+    dp.register_callback_query_handler(
+        student_group_menu,
+        callbacks.group_callback.filter(role="student"),
+        state=states.Student.show_groups
+    )
+
+    dp.register_callback_query_handler(
+        show_not_done_labs_files,
+        callbacks.show_callback.filter(data_type="lab", user_role="student"),
+        state=states.Student.group_menu
     )
     logger.info('Student handlers are successfully registered')
