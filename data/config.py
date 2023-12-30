cf58f95217a9720faefb399964c0fa31d93b3409
Алан Газиев <alan.gaziev27@gmail.com>
2023-12-30 23:42:25 +0200

    config: refactor, add dbcreds class and erase private prefix

--- data/config.py
+++ data/config.py
@@ -7,26 +7,26 @@ class Config:
     def __init__(self) -> None:
         dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
         load_dotenv(dotenv_path, override=True)
-        self._bot_token = os.getenv('BOT_TOKEN')
-        self._cloud_drive_token = os.getenv('YANDEX_DISK_TOKEN')
-        self._admins = self._get_admins()
-        self._database_connection_parameters = self._get_database_connection_parameters()
+        self.bot_token = os.getenv('BOT_TOKEN')
+        self.cloud_drive_token = os.getenv('YANDEX_DISK_TOKEN')
+        self.admins = self._get_admins()
+        self.database_connection_parameters : DBCreds = DBCreds()
 
     @property
     def bot_token(self) -> str:
-        return self._bot_token
+        return self.bot_token
 
     @property
     def cloud_drive_token(self) -> str:
-        return self._cloud_drive_token
+        return self.cloud_drive_token
 
     @property
     def database_connection_parameters(self) -> dict:
-        return self._database_connection_parameters
+        return self.database_connection_parameters
 
     @property
     def admins(self) -> tuple[int, ...]:
-        return self._admins
+        return self.admins
 
     def _get_admins(self) -> tuple:
         try:
@@ -38,18 +38,10 @@ class Config:
 
         return admins
 
-    def _get_database_connection_parameters(self) -> dict:
-        dotenv_variables = ('DB_USER', 'DB_USER_PASSWORD',
-                            'DB_HOST', 'DB_PORT', 'DB_NAME')
-        keys = ('user', 'password', 'host', 'port', 'database')
-        pairs = zip(keys, dotenv_variables)
-        parameters = {}
-        for key, dotenv_variable in pairs:
-            try:
-                parameters.update({key: os.getenv(dotenv_variable)})
-            except AttributeError:
-                logger.error(
-                    f"{dotenv_variable} environment variable is not set")
-                parameters.update({key: None})
-
-        return parameters
+class DBCreds():
+    def __init__(self):
+        user = os.getenv('DB_USER')
+        password = os.getenv('DB_USER_PASSWORD')
+        host = os.getenv('DB_HOST')
+        port = os.getenv('DB_PORT')
+        database = os.getenv('DB_NAME')
