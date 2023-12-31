ac7656bbc3b7f95be0d62c067c89aa37bac1745d
Алан Газиев <alan.gaziev27@gmail.com>
2023-12-31 16:49:49 +0200

    new inserter: methods for group creating

---
+++ database/inserter.py
@@ -0,0 +1,27 @@
+from handlers.error import error_handling
+from utils import Group, GroupMember
+
+
+class Inserter:
+    @staticmethod
+    def create_group(name, teacher_id):
+        try:
+            Group.create(name=name, teacher_id=teacher_id).save()
+        except Exception as e:
+            error_handling(e,
+                           f"Не смогли зарегистрировать группу {name} для учителя {teacher_id}")
+            return False
+        else:
+            return Group
+
+    @staticmethod
+    def add_group_members_to_group(group: Group, membersNames: list):
+        for memberName in membersNames:
+            try:
+                GroupMember.create(group=group, name=memberName, user=None).save()
+            except Exception as e:
+                error_handling(e,
+                               f"Не смогли зарегистрировать участника группы {group.name} "
+                               f"с айди {group.id} с именем {memberName}")
+                return False
+        return True
