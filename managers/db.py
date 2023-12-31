e5ba3257e2b3e00cdeb9a9aeb788deb5e204c58e
Алан Газиев <alan.gaziev27@gmail.com>
2023-12-31 16:49:22 +0200

    db manager: now without instance required

--- managers/db.py
+++ managers/db.py
@@ -1,21 +1,9 @@
-from loguru import logger
-from database import Inserter, Selector, Updater, Deleter
+from openpyxl.pivot.cache import GroupMembers
+from psycopg2 import IntegrityError
 
+from database.inserter import Creator
+from utils.models import *
+from handlers.error import error_handling
 
-class DatabaseManager(Inserter, Selector, Updater, Deleter):
-    """Class representing database manager, which contains all database operations\n
-        CRUD operations are separated into different classes\n
-
-    Args:
-        Inserter (class): CREATE
-        Selector (class): READ
-        Updater (class): UPDATE
-        Deleter (class): DELETE
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        logger.debug("Database manager object was initialized")
-
-
-database_manager = DatabaseManager()
+class DatabaseManager(Creator):
+    pass
