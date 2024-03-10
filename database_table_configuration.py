b0d7d5288d25f1c39794b7c44cc1486ae1505b97
Алан Газиев <alan.gaziev27@gmail.com>
2024-03-10 21:31:39 +0200

    db config with drop and some test rows

--- database_table_configuration.py
+++ database_table_configuration.py
@@ -1,14 +1,17 @@
 def configure_and_fill_database_tables() -> None:
     from repositories import db
     import utils.models
-    db.create_tables([utils.models.User,
+    alltables = [utils.models.User,
                       utils.models.Teacher,
                       utils.models.Group,
                       utils.models.GroupMember,
                       utils.models.LabRegistry,
                       utils.models.Status,
-                      utils.models.LabWork])
-
+                      utils.models.LabWork]
+    #db.drop_tables(alltables, safe=False)
+    db.create_tables(alltables)
+    utils.models.User(telegram_id="292667494", username="vcdddk").save()
+    utils.models.Teacher(first_name="Alan", last_name="Gazziev", patronymic="gazievich",user="292667494").save()
 
 if __name__ == '__main__':
     configure_and_fill_database_tables()
