becad96c0b02b4cbc63620bff0ec7b95802b9b7a
Алан Газиев <alan.gaziev27@gmail.com>
2024-03-10 21:31:24 +0200

    enum for LabStatus title

--- utils/enums.py
+++ utils/enums.py
@@ -5,3 +5,11 @@ class Blocked(enum.Enum):
     FALSE = 'FALSE'
     TRUE = 'TRUE'
     ANY = 'TRUE, FALSE'
+
+
+class LabStatus(enum.Enum):
+    HandOver = "Сдано"
+    Rejected = "Отклонено"
+    NotChecked = "Не проверено"
+    All = "*"
+
