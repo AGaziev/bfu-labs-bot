9cbacba41f35913140b9f1a840a579024be72a47
Алан Газиев <alan.gaziev27@gmail.com>
2024-03-10 21:24:46 +0200

    init files : fix import problems

--- managers/__init__.py
+++ managers/__init__.py
@@ -1,4 +1,3 @@
 from .group import GroupManager
 from .lab import LabManager
-from .db import DatabaseManager, database_manager
 from .cloud import CloudManager
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .states import *
 from .models import *
 from .parsers import TxtParser, CsvParser
+from .formatter import Formatter
