0f2d550b37e83d38e847f8295722331aecb4090d
Алан Газиев <alan.gaziev27@gmail.com>
2024-04-25 21:05:39 +0200

    lab statuses filler db

--- database_table_configuration.py
+++ database_table_configuration.py
@@ -1,3 +1,6 @@
+from utils.enums import LabStatus
+
+
 def configure_and_fill_database_tables() -> None:
     from repositories import db
     import utils.models
@@ -10,6 +13,12 @@ def configure_and_fill_database_tables() -> None:
                       utils.models.LabWork]
     #db.drop_tables(alltables, safe=False)
     db.create_tables(alltables)
+    lab_statuses = [
+        {"title": LabStatus.HANDOVER.value},
+        {"title": LabStatus.NOTCHECKED.value},
+        {"title": LabStatus.REJECTED.value},
+    ]
+    utils.models.Status.insert_many(lab_statuses).execute()
     utils.models.User(telegram_id="292667494", username="vcdddk").save()
     utils.models.Teacher(first_name="Alan", last_name="Gazziev", patronymic="gazievich",user="292667494").save()
 
