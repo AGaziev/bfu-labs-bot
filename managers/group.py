67001197c16ac90cda261ae2fa7692e7c5d9212d
Алан Газиев <alan.gaziev27@gmail.com>
2024-04-25 21:08:35 +0200

    adapting group to new selector

--- managers/group.py
+++ managers/group.py
@@ -20,7 +20,7 @@ class GroupManager:
         CloudManager.create_group_folder(name)
         folder_url = CloudManager.get_group_folder_link(name)
         # create in db
-        teacher = DatabaseManager.select_teacher_credentials_by_telegram_id(teacher_user_id)
+        teacher = DatabaseManager.get_teacher_by_telegram_id(teacher_user_id)
         group = DatabaseManager.create_group(name, teacher.id)
         DatabaseManager.add_group_members_to_group(group, students)
         return folder_url, group.id
@@ -63,19 +63,20 @@ class GroupManager:
         return ids_and_credentials
 
     @staticmethod
-    def get_groups_for_student(telegram_id: int) -> list[tuple[int, str]]:
+    def get_groups_for_student(telegram_id: int) -> list[Group]:
         user = DatabaseManager.get_user_by_telegram_id(telegram_id)
         student_groups = DatabaseManager.select_user_groups_names_with_id(user)
         return student_groups
 
     @staticmethod
     def is_student_already_connected(telegram_id: int, group_id: int) -> bool:
-        users_in_group = DatabaseManager.select_registered_members_from_group(group_id)
+        group = DatabaseManager.get_group_by_id(group_id)
+        users_in_group = DatabaseManager.select_type_members_for_group(group, True)
         return telegram_id in users_in_group
 
     @staticmethod
     def add_lab_to_db(group_id: int, lab_name: str, lab_link: str):
-        new_lab_number = DatabaseManager.select_all_labs_count_from_group(group_id)+1
+        new_lab_number = DatabaseManager.select_all_labs_count_from_group(group_id) + 1
         if (DatabaseManager.add_new_lab_to_group(group=DatabaseManager.get_group_by_id(group_id),
                                                  lab_descr=lab_name,
                                                  lab_link=lab_link,
@@ -124,22 +125,22 @@ class GroupManager:
         return passed, rejected, not_checked, labs_at_all
 
     @staticmethod
-    def get_group_info(group_id: int, group_name: str):
+    def get_group_info(group_id: int):
+        group = DatabaseManager.get_group_by_id(group_id)
         group_info = GroupInfo()
-        group_info.registered_members_count = GroupManager.get_count_of_registered_members_from_group(group_id=group_id)
-        group_info.unregistered_members_count = GroupManager.get_count_of_unregistered_members_from_group(
-            group_name=group_name)
+        group_info.registered_members_count = GroupManager.get_count_of_type_members_from_group(group_id, True)
+        group_info.unregistered_members_count = GroupManager.get_count_of_type_members_from_group(group_id, False)
         group_info.students_at_all = group_info.registered_members_count + \
                                      group_info.unregistered_members_count
-        group_info.lab_condition_files_count = GroupManager.select_lab_condition_files_count_from_group(
-            group_id=group_id)
-        group_info.passed_labs_count, group_info.rejected_labs_count, group_info.not_checked_labs_count, group_info.labs_at_all = GroupManager.select_students_labs_statuses_count_from_group(
-            group_id=group_id)
+        group_info.lab_condition_files_count = len(DatabaseManager.select_labs_for_group(group))
+        group_info.passed_labs_count, group_info.rejected_labs_count, group_info.not_checked_labs_count, group_info.labs_at_all = \
+            GroupManager.select_students_labs_statuses_count_from_group(group_id)
         return group_info
 
     @staticmethod
     def get_group_stats_file(group_id: int):
-        stats = DatabaseManager.select_lab_stats_by_whole_group(group_id)
+        group = DatabaseManager.get_group_by_id(group_id)
+        stats = DatabaseManager.select_lab_stats_by_whole_group(group)
         group_for_stats = DatabaseManager.get_group_by_id(group_id)
         group_name = group_for_stats.name
         lab_number = DatabaseManager.select_all_labs_count_from_group(group_id)
@@ -147,6 +148,7 @@ class GroupManager:
         for name, labs in itertools.groupby(stats, key=lambda x: x.name):
             labs = list(labs)
             info_for_generator[name] = [bool(labs[0].is_registered),
+                                        # bc any of elements in "labs" will have equal "is_registered"
                                         [[lab.number, lab.date, lab.status] for lab in labs if lab.number is not None]]
         if info_for_generator:
             return StatsGenerator.generate_stats(group_name, info_for_generator, lab_number)
@@ -168,8 +170,10 @@ class GroupManager:
     @staticmethod
     def post_lab_from_student(group_name: str, telegram_id: int, lab_number: int, lab_file: BytesIO,
                               file_extension: str) -> None:
-        student_credentials = DatabaseManager.select_student_credentials(telegram_id=telegram_id, group_name=group_name)
-        lab_name, lab_id = DatabaseManager.select_lab_name_and_id_by_number(group_name=group_name,
+        group = DatabaseManager.get_group_by_name(group_name)
+        student_credentials = DatabaseManager.get_group_member_by_telegram_and_group(telegram_id=telegram_id,
+                                                                                     group_id=group.id)
+        lab_name, lab_id = DatabaseManager.get_lab_by_id(group_name=group_name,
                                                                             lab_number=lab_number)
         lab_name = f'{lab_name}.{file_extension}'
 
